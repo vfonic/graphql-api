230944164b4827567d55d65be3f3661e23c339bb
Colin Walker <colinwalker270@gmail.com>
2016-10-01 13:26:37 -0700

    release version 0.1.3

--- bin/push.sh
+++ bin/push.sh
@@ -4,6 +4,7 @@ ver=$1
 
 > lib/graphql/api/version.rb
 printf "module GraphQL\n  module Api\n    VERSION = '${1}'\n  end\nend\n" >> lib/graphql/api/version.rb
+git commit -am "release version ${1}"
 
 gem build graphql-api.gemspec
 gem push graphql-api-${1}.gem
